bdb72284728c45b719b023a138e024816ee8926c
Bryan Whitman <BW.Hitman89@gmail.com>
2020-09-22 08:18:26 -0500

    ,Added in the Introduction

--- TheHauntedHouse/TheHauntedHouse/Program.cs
+++ TheHauntedHouse/TheHauntedHouse/Program.cs
@@ -6,8 +6,16 @@ namespace TheHauntedHouse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Welcome to my Game");
+            Console.WriteLine("______A Haunted House______");
+            Console.WriteLine("Created by: Bryan Whitman");
+            Console.WriteLine("Welcome to my Game!");
+
+            Console.WriteLine("Note: as you move through the game, you will be presented with a series of options.");
+            Console.WriteLine("When prompted, simply type the letter you want, and hit enter/return");
+            Console.WriteLine("Don't get to scared!");
+
+
+
         }
     }
 }
