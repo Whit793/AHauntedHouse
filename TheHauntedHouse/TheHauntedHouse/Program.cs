bc4e3cba2fad44efb8ed909efdd76f298e643d70
Bryan Whitman <BW.Hitman89@gmail.com>
2020-09-22 08:12:33 -0500

    ,Beginning step

---
+++ TheHauntedHouse/TheHauntedHouse/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TheHauntedHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to my Game");
+        }
+    }
+}
